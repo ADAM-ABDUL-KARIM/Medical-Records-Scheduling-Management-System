3db4817ed61fa0c2e93fe3653515fb9adca4fe09
Adam <aha023@pu.edu.lb>
2026-01-29 17:30:46 +0200

    Front end Vercel URL connection

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -105,7 +105,7 @@ function Home() {
           </Link>
         </div>
         <div>
-          <Link to="http://127.0.0.1:8000/admin/" target="_blank">
+          <Link to="https://medical-records-scheduling-manageme.vercel.app/admin/" target="_blank">
             <img src={adminpanel} alt="Admin Panel" width={150} height={150} />
             <span className="hiddenContainer">Admin Panel</span>
           </Link>
