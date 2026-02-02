9d5eeee69620f2277f5a7a604e815aef939c6668
Adam <aha023@pu.edu.lb>
2026-02-02 15:45:21 +0200

    fixed RBAC

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,24 @@ import Form from "../components/Form";
 
 function Login() {
 
-  return <Form route="/api/token/" method="login" />;
+  return (
+  <>
+
+   <div style={{ textAlign: "left ", marginTop: "50px", marginLeft: "70px" }}>
+        <h1>Welcome Recruiters </h1>
+        <p style={{ fontSize: "16px", color: "#555", maxWidth: "500px", margin: "10px auto" }}>
+          This is a demo of my full-stack healthcare application. You can log in using the default credentials below to explore patient and admin features safely.
+        </p>
+        <p style={{ fontSize: "14px", color: "#777" }}>
+          <strong>Admin:</strong> admin / admin<br />
+          <strong>Patient:</strong> defaultUsername / defaultPassword
+        </p>
+
+        </div>
+  <Form route="/api/token/" method="login" />;
+
+  </>
+  );
 }
 
 export default Login;
